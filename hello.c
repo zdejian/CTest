88919fddb2ec59a76fd8df8de54e98e8f41a6ce1
zhangdejian <2220191174@qq.com>
2021-10-05 01:03:38 +0800

    v12版本，master branch

--- hello.c
+++ hello.c
@@ -5,6 +5,10 @@ int main(int argc, char *argv[])
 {
     printf("1 + 2 = %d\n", sum(1, 2));
     printf("1 + 2 = %d\n", sum(1, 2));
+    printf("1 + 2 = %d\n", sum(1, 2));
+    printf("hello world!\n");    
+    printf("hello world!\n");    
+    printf("1 + 2 = %d\n", sum(1, 2));
     printf("hello world!\n");    
     printf("hello world!\n");    
     return 1;
