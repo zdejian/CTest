699548c30e727471011b32e8a64eaed88b1b3891
zhangdejian <2220191174@qq.com>
2021-10-04 23:27:11 +0800

    v7版本

--- hello.c
+++ hello.c
@@ -8,5 +8,9 @@ int main(int argc, char *argv[])
     printf("hello world!\n");    
     printf("hello world!\n");    
     printf("hello world!\n");    
+    printf("hello world!\n");    
+    printf("hello world!\n");    
+    printf("hello world!\n");    
+    printf("hello world!\n");    
     return 1;
 }
