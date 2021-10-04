27dce9e5a23027b4999cc245e10934727f6e2a7e
zhangdejian <2220191174@qq.com>
2021-10-05 00:37:40 +0800

    v10版本 master branch

--- hello.c
+++ hello.c
@@ -5,7 +5,5 @@ int main(int argc, char *argv[])
 {
     printf("1 + 2 = %d\n", sum(1, 2));
     printf("hello world!\n");    
-    printf("hello world!\n");    
-    printf("hello world!\n");    
     return 1;
 }
