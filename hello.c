2533dd662dd377a3678791be24b996e0fe4cba4e
zhangdejian <2220191174@qq.com>
2021-10-05 12:27:01 +0800

    v15版本，修改hello.c内容

--- hello.c
+++ hello.c
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
     printf("1 + 2 = %d\n", sum(1, 2));
+    printf("1 + 2 = %d\n", sum(1, 2));
+    printf("hello world!\n");    
     printf("hello world!\n");    
     return 1;
 }
